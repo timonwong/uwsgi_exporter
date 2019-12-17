7da74eddeb9479e6b8e3a09dfd24c8dcc625a751
Vladimir Zhukov <vzhukov@ivi.ru>
2019-12-17 13:04:02 +0300

    Fix flag collect.cores

--- uwsgi_exporter.go
+++ uwsgi_exporter.go
@@ -11,23 +11,25 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+var (
+	listenAddress = kingpin.Flag("web.listen-address", "Address on which to expose metrics and web interfaces.").Default(":9117").String()
+	metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+	statsURI      = kingpin.Flag("stats.uri", "URI for accessing uwsgi stats.").Default("").String()
+	statsTimeout  = kingpin.Flag("stats.timeout", "Timeout for trying to get stats from uwsgi.").Default("5s").Duration()
+	collectCores  = kingpin.Flag("collect.cores", "Collect cores information per uwsgi worker.").Default("false").Bool()
+)
+
 func init() {
 	prometheus.MustRegister(version.NewCollector("uwsgi_exporter"))
-}
-
-func main() {
-	var (
-		listenAddress = kingpin.Flag("web.listen-address", "Address on which to expose metrics and web interfaces.").Default(":9117").String()
-		metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
-		statsURI      = kingpin.Flag("stats.uri", "URI for accessing uwsgi stats.").Default("").String()
-		statsTimeout  = kingpin.Flag("stats.timeout", "Timeout for trying to get stats from uwsgi.").Default("5s").Duration()
-		collectCores  = kingpin.Flag("collect.cores", "Collect cores information per uwsgi worker.").Default("true").Bool()
-	)
 
 	log.AddFlags(kingpin.CommandLine)
 	kingpin.Version(version.Print("uwsgi_exporter"))
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
+
+}
+
+func main() {
 
 	log.Infoln("Starting uwsgi_exporter", version.Info())
 	log.Infoln("Build context", version.BuildContext())
